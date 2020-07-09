2609505c07ec6c2fa2c2fb41f09bc0a7d2a82483
auna <annah.ramones@gmail.com>
2020-07-09 14:02:08 -0700

    [annah] dark mode toggle working!

--- src/app/components/dark-mode-toggle-menu/dark-mode-toggle-menu.component.ts
+++ src/app/components/dark-mode-toggle-menu/dark-mode-toggle-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-dark-mode-toggle-menu',
@@ -7,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DarkModeToggleMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   
+  }
 
   ngOnInit() {}
 
+  onClick(event){
+    let systemDark = window.matchMedia("(prefers-color-scheme: dark)");
+    systemDark.addListener(this.colorTest);
+    if(event.detail.checked){
+      document.body.setAttribute('data-theme', 'dark');
+    }
+    else{
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
+   colorTest(systemInitiatedDark) {
+    if (systemInitiatedDark.matches) {
+      document.body.setAttribute('data-theme', 'dark');		
+    } else {
+      document.body.setAttribute('data-theme', 'light');
+    }
+  }
+
+  
 }
